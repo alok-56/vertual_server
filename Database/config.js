8252932965701fd9ba54844609d34074b11dd1e0
alok-56 <alokkumar11746@gmail.com>
2023-04-13 02:38:17 +0530

    alok

--- Database/config.js
+++ Database/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Health', {
+mongoose.connect('mongodb+srv://dosti1166:ZQVKE6RJ4MommQeq@cluster0.huk0pb3.mongodb.net/?retryWrites=true&w=majority', {
     family: 4,
 }).then((res) => {
     console.log("connected to database")
--- controller/user/get.js
+++
@@ -1,6 +0,0 @@
-
-const geet=async(req,res)=>{
-    res.send("alok")
-}
-
-module.exports=geet;
